6ec75927877e237445b28a5fd21b0da6e0a8fae0
apage-pi <72623953+apage-pi@users.noreply.github.com>
2022-11-04 19:13:26 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #/bin/bash
-apt install gcc make
-wget https://raw.githubusercontent.com/apage-pi/linux.unify/main/LinuxUnifyLogoLg.png
-wget https://raw.githubusercontent.com/apage-pi/linux.unify/main/LinuxUnifyLogoSm.png
-cp LinuxUnifyLogoSm.png /usr/share/plymouth/themes/spinner/bgrt-fallback.png
-cp LinuxUnifyLogoLg.png /usr/share/plymouth/themes/spinner/watermark.png
-cp LinuxUnifyLogoLg.png /usr/share/plymouth/ubuntu-logo.png
+file = /luupdate/22.10.1.txt
+if [ -f "$file" ]; then
+  echo "System already version 22.10.1. Skipping Update."
+else
+  mkdir /luupdate
+  apt install gcc make
+  wget https://raw.githubusercontent.com/apage-pi/linux.unify/main/LinuxUnifyLogoLg.png
+  wget https://raw.githubusercontent.com/apage-pi/linux.unify/main/LinuxUnifyLogoSm.png
+  cp LinuxUnifyLogoSm.png /usr/share/plymouth/themes/spinner/bgrt-fallback.png
+  cp LinuxUnifyLogoLg.png /usr/share/plymouth/themes/spinner/watermark.png
+  cp LinuxUnifyLogoLg.png /usr/share/plymouth/ubuntu-logo.png
+  touch /luupdate/22.10.1.txt
+  echo "Update Complete. System now version 22.10.1."
+fi
