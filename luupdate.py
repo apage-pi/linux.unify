0424451acad50433877fe54d39b6fda62a7ef6c5
apage-pi <72623953+apage-pi@users.noreply.github.com>
2022-11-06 08:35:36 -0600

    Update luupdate.py

--- luupdate.py
+++ luupdate.py
@@ -10,7 +10,7 @@ def update():
     if new == old:
         updater.info("Linux.Unify Updater", "System already version 22.10.2. Skipped Update.")
     else:
-        updater.info("Linux.Unify Updater", "System updated to 22.10.2. Update Complete.")
+        updater.info("Linux.Unify Updater", "Update Complete.")
 updater = App(title="Linux.Unify System Manager")
 explain = Text(updater, text="Select an action:")
 updatebutton = PushButton(updater, text="Update", command=update)
